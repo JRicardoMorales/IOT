b3209b00936f2328afe48c6d246ab4a6866ef9ce
Jose Ricardo Morales Sanchez <139749932+JRicardoMorales@users.noreply.github.com>
2024-06-21 12:38:31 +0200

    Delete Proyecto DAD directory

--- Proyecto DAD/Contenido Eclipse/src/main/java/es/us/dad/mysql/entities/ActuatorType.java
+++
@@ -1,8 +0,0 @@
-package es.us.dad.mysql.entities;
-
-/**
- * Tipos de actuadores soportados por el framework.
- */
-public enum ActuatorType {
-	Light, Relay, Fan, Motor, LinearActuator, WaterPump, IrrigationPump
-}
--- Proyecto DAD/Contenido Eclipse/src/main/java/es/us/dad/mysql/entities/SensorType.java
+++
@@ -1,8 +0,0 @@
-package es.us.dad.mysql.entities;
-
-/**
- * Tipos de sensores soportados por el framework.
- */
-public enum SensorType {
-	Temperature, Humidity, Pressure, Luminosity, SoilHumidity, SunIrradiation, Power, EnergyConsumption, AirQuality
-}
